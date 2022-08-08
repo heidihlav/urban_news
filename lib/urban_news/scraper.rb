d1c1c98314071a8de508662a0870e843db573b2a
Heidi Hlavinka <hchlav@gmail.com>
2022-08-08 15:58:27 -0500

    Made class definition that gets Kinder  institute intro.

--- lib/urban_news/scraper.rb
+++ lib/urban_news/scraper.rb
@@ -1,11 +1,18 @@
-class UrbanNews::Scraper
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
 
-    def kinder_intro
+module UrbanNews
+    class Scraper
+
+    def self.kinder_intro
       doc = Nokogiri::HTML(URI.open("https://kinder.rice.edu/issues"))
-      intro = doc.css("div .main div .content").text
-
+      intro = doc.css("div .main div .content").text.split("    ")
+      puts "#{intro[3]}" 
+      puts "#{intro[4]}"
     end
 
-
-
 end
+end
+
+UrbanNews::Scraper.kinder_intro
