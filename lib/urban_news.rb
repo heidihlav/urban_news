4c136dc891c1887e12f26d80639b8b19c59a0915
Heidi Hlavinka <hchlav@gmail.com>
2022-08-08 15:14:58 -0500

    Added required files and required gems to environment file

--- lib/urban_news.rb
+++ lib/urban_news.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 require_relative "urban_news/version"
+require_relative "urban_news/cli"
+require_relative "urban_news/month"
+require_relative "urban_news/event"
+require_relative "urban_news/scraper"
+
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
 
 module UrbanNews
   class Error < StandardError; end
--- lib/urban_news/scraper.rb
+++ lib/urban_news/scraper.rb
@@ -1,5 +1,10 @@
 class UrbanNews::Scraper
-    
+
+    def kinder_intro
+      doc = Nokogiri::HTML(URI.open("https://kinder.rice.edu/issues"))
+      intro = doc.css("div .main div .content").text
+
+    end
 
 
 
