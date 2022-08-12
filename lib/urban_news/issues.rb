1beb903e64136a1c4002d8d833eac1b402a074d3
Heidi Hlavinka <hchlav@gmail.com>
2022-08-12 15:55:51 -0500

    Created url array with links to each issue page. Not DRY but gets it working.

--- lib/urban_news/issues.rb
+++ lib/urban_news/issues.rb
@@ -22,12 +22,28 @@ module UrbanNews
         end 
     end
 
-    def self.user_select_an_issue
-      self.print_all_issues.each do |i|
-        HTML(URI.open("https://kinder.rice.edu/issues"))
-      
-
+    def issue_urls
+     @url = ["https://kinder.rice.edu/issue/covid-19-and-cities",
+      "https://kinder.rice.edu/issue/demographics",
+      "https://kinder.rice.edu/issue/economic-development",
+      "https://kinder.rice.edu/issue/education", 
+      "https://kinder.rice.edu/issue/elections",
+      "https://kinder.rice.edu/issue/governance",
+      "https://kinder.rice.edu/issue/health", 
+      "https://kinder.rice.edu/issue/housing",
+      "https://kinder.rice.edu/issue/hurricane-harvey",
+      "https://kinder.rice.edu/issue/immigration", 
+      "https://kinder.rice.edu/issue/sun-belt",
+      "https://kinder.rice.edu/issue/transportation",
+      "https://kinder.rice.edu/issue/urban-disparity",
+      "https://kinder.rice.edu/issue/urban-planning"]
     end
+        # puts "#{i}"
+        # .collect do |url|
+        #  puts url 
+      # HTML(URI.open("https://kinder.rice.edu/issue" + "#{self.issue_page_url}" ))
+        # end
+      # end
 
     # def self.all_issue_pages
     #   @url = UrbanNews::Scraper.issue_page_url
@@ -41,26 +57,12 @@ module UrbanNews
     #   self.print_all_issues
     #     self.all_issue_pages
     #     "#{name}" "#{url}" 
-    #     (href="https://kinder.rice.edu/issue/covid-19-and-cities")
-    #     (href="https://kinder.rice.edu/issue/demographics")
-        # <a href="https://kinder.rice.edu/issue/economic-development" target="_self"></a>
-        # <a href="https://kinder.rice.edu/issue/education" target="_self"></a>
-        # <a href="https://kinder.rice.edu/issue/elections" target="_self"></a>
-        # <a href="https://kinder.rice.edu/issue/governance" target="_self"></a>
-        # <a href="https://kinder.rice.edu/issue/health" target="_self"></a>
-        # <a href="https://kinder.rice.edu/issue/housing" target="_self"></a>
-        # <a href="https://kinder.rice.edu/issue/hurricane-harvey" target="_self"></a>
-        # <a href="https://kinder.rice.edu/issue/immigration" target="_self"></a>
-        # <a href="https://kinder.rice.edu/issue/sun-belt" target="_self"></a>
-        # <a href="https://kinder.rice.edu/issue/transportation" target="_self"></a>
-        # <a href="https://kinder.rice.edu/issue/urban-disparity" target="_self"></a>
-        # <a href="https://kinder.rice.edu/issue/urban-planning" target="_self"></a>
+    #     
 
 
 
 
 
-    # end
 
 
     # def self.user_choose_issue
