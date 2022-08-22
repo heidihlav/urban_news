70cc0b05b622d598f1905732526007234a7770d7
Heidi Hlavinka <hchlav@gmail.com>
2022-08-21 19:44:51 -0500

    Require story.rb in environnment file.

--- config/environment.rb
+++ config/environment.rb
@@ -9,10 +9,9 @@ require 'pry'
 require 'nokogiri'
 require 'open-uri'
 
-require_relative "../lib/urban_news/article"
 require_relative "../lib/urban_news/cli"
-require_relative "../lib/urban_news/issues"
 require_relative "../lib/urban_news/scraper"
+require_relative "../lib/urban_news/story"
 require_relative "../lib/urban_news/version"
 
 
