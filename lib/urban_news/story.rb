8619d75797cc777a34c2e27c35a11cfe6e5acdcd
Heidi Hlavinka <hchlav@gmail.com>
2022-08-22 09:06:57 -0500

    Initialized story class.

---
+++ lib/urban_news/story.rb
@@ -0,0 +1,17 @@
+module UrbanNews
+  class Story
+
+    attr_accessor :title, :description, :metadata
+
+    def initialize(title, description, metadata)
+      @title = title
+      @description = description
+      @metadata = metadata 
+    end
+
+
+
+
+        
+  end
+end
