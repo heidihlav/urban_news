d9c453c6bdbe43c9371e0b5c23f28b1201f079fd
Heidi Hlavinka <hchlav@gmail.com>
2022-08-22 20:01:36 -0500

    Latest story scraped.

--- lib/urban_news/scraper.rb
+++ lib/urban_news/scraper.rb
@@ -20,16 +20,15 @@ module UrbanNews
       title = doc.css("#block-views-blog-posts-top .item .item-title").text
       description = doc.css("#block-views-blog-posts-top .item .item-description").text
       metadata = doc.css("#block-views-blog-posts-top .item .item-meta-content").text
+      UrbanNews::Story.new(title, description, metadata)
 
     end
-
     def self.get_latest_story
-      doc = Nokogiri::HTML(URI.open("https://kinder.rice.edu/"))
+      doc = Nokogiri::HTML(URI.open("https://kinder.rice.edu/urban-edge/"))
       title = doc.css(".featured-item.Urban.Edge .item-title").text
       description = doc.css(".featured-item.Urban.Edge .item-description").text
-      # metadata = doc.css(".item-meta-content").text
-      "#{title}" + "#{description}" 
-      binding.pry
+      metadata = doc.css(".featured-item.Urban.Edge .item-meta-content").text
+      "#{title}" "#{description}" "#{metadata}"
     end
 
 
