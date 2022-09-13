ba50a516396cdc6c82d1b581d4e2a0638b246ca0
Heidi Hlavinka <hchlav@gmail.com>
2022-09-13 11:14:31 -0500

    Combined scraping story pages and blog home page in one method.

--- lib/urban_news/scraper.rb
+++ lib/urban_news/scraper.rb
@@ -12,32 +12,23 @@ module UrbanNews
     end
 
     def scrape_blog_page
-      blog_page = self.get_page
-   # blog_page = Nokogiri::HTML(URI.open('https://kinder.rice.edu/urban-edge/'))
-      teasers = []
+    blog_page = Nokogiri::HTML(URI.open('https://kinder.rice.edu/urban-edge/'))
+      stories = []
       blog_page.css('#block-views-blog-posts-top .view-content').each do |t|
         t.css('.item').each do |i|
-          # teaser_url = puts blog_page.css('#block-views-blog-posts-top .item a').map { |u| "https://kinder.rice.edu#{u['href']}" }.collect {|f| puts f }
           teaser_description = i.css('.item-description').text
           teaser_title = i.css('.item-title').text
           teaser_metadata = i.css('.date-display-single').children.text
-          teasers << { title: teaser_title, description: teaser_description, metadata: teaser_metadata }
-        end
-      end
-      teasers
-    end
-    
-    def scrape_story_pages
-      stories = []
-      story_pages = Nokogiri::HTML(URI.open('https://kinder.rice.edu/urban-edge/'))
-        links = story_pages.css('#block-views-blog-posts-top .item a').map { |u| "https://kinder.rice.edu#{u['href']}" }
+          links = i.css('a').map { |u| "https://kinder.rice.edu#{u['href']}" }
           links.each do |link|
           story_url = link
           open_link = Nokogiri::HTML(URI.open(link))
           story_content = open_link.css('div.content').text.strip
-       stories << { url: story_url, content: story_content }
-     end
-    stories
+          puts stories << { title: teaser_title, description: teaser_description, metadata: teaser_metadata, url: story_url, content: story_content } 
+            binding.pry
+        end
+        end
+      end
     end
     
     def make_stories
@@ -46,11 +37,11 @@ module UrbanNews
       end
     end
 
-    def make_content
-      self.scrape_story_pages.each do |c|
-        UrbanNews::Story.new_from_story_pages(c)
-      end
-    end
+    # def make_content
+    #   self.scrape_story_pages.each do |c|
+    #     UrbanNews::Story.new_from_story_pages(c)
+    #   end
+    # end
   
     def kinder_intro #gsub extra spaces btwn paragraphs?
       doc = Nokogiri::HTML(URI.open("https://kinder.rice.edu/issues"))
--- lib/urban_news/story.rb
+++ lib/urban_news/story.rb
@@ -5,29 +5,29 @@ require 'pry'
 module UrbanNews
   class Story
 
-    attr_accessor :title, :description, :metadata, :latest_story
+    attr_accessor :title, :description, :metadata, :url, :content
     @@all = []
 
-
     def self.new_from_blog_page(s)
-      self.new(s[:title], s[:description], s[:metadata])
+      self.new(s[:title], s[:description], s[:metadata], s[:url], s[:content)
     end
 
-    def self.new_from_story_pages(c)
-      self.new(c[:url], c[:content])
-    end
+    # def self.new_from_story_pages(c)
+    #   [c[:url], c[:content]]
+    # end
 
     def initialize(title, description, metadata, url, content)
       @title = title
       @description = description
       @metadata = metadata
+      @url = url
+      @content = content
       @@all << self
     end
 
     def self.all
       @@all
     end
-
 
     def self.top_stories
       self.all.each.with_index do |story, index|
@@ -38,7 +38,6 @@ module UrbanNews
       end
     end
 
-
     def self.top_stories_content
       self.top_stories.with_index do |story, index|
         binding.pry
@@ -47,7 +46,6 @@ module UrbanNews
         puts "#{story.content}"
        end
     end
-
 
     def self.latest_story
       @latest_story = UrbanNews::Scraper.get_latest_story   
